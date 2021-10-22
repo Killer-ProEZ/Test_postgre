0a8170e3a6aaa1d72684a50ddc5cf370782f69cb
Killer-ProEZ <ht01646@gmail.com>
2021-10-22 10:43:04 +0700

    v0.1

---
+++ index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>App</title>
+</head>
+
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">UserName</th>
+                <th scope="col">CustomerName</th>
+                <th scope="col">Tel</th>
+            </tr>
+        </thead>
+        <?php
+            include_once('connection.php');
+            $result=pg_query("select * from public.customer");
+            while($row=pg_fetch_array($result,PGSQL_ASSOC){
+        ?>
+        <tbody>
+            <tr>
+                <td><?php echo $row['username'];?></td>
+                <td><?php echo $row['customername'];?></td>
+                <td><?php echo $row['tel'];?></td>
+            </tr>
+        </tbody>
+        <?php>
+            }
+        <?>
+    </table>
+
+</body>
+
+</html>
